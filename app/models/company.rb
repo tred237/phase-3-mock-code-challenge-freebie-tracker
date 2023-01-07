79c1584f17f499ada6dc9cdf77b75c294d966756
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-06 23:58:16 -0700

    commit tree 849bbb8fa4c0c183c7e7f53572d1142e23fc47a5

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,13 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(attributes)
+        self.freebies << Freebie.create(item_name: attributes[:item_name], value: attributes[:value], dev: attributes[:dev])
+    end
+
+    def self.oldest_company
+        min_year = Company.all.minimum('founding_year')
+        Company.find_by("founding_year = #{min_year}")
+    end
 end
